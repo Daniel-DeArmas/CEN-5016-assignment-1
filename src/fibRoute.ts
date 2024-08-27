bd8a4910e9ee5eded83fc37c600f6c54f9ea77e2
Daniel-DeArmas <ddearmas2017@gmail.com>
2024-08-27 06:17:20 -0400

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -6,7 +6,7 @@ export default (req: { params: string; }, res: { send: (arg0: string) => void; }
   const num:string = req.params;
 
   const fibN:number = fibonacci(parseInt(num));
-  let result:string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
